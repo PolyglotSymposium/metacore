f857dde02ab6219438efcbfc91043084f6dabb3b
Kazark <kazark@zoho.com>
2020-07-18 14:13:09 -0400

    feat: re-implement eval arg passed on cmd line

--- main.c
+++ main.c
@@ -9,22 +9,34 @@ int main(int argc, char* argv[])
   Map* env = NULL;
   char* code = NULL;
   int cont = true;
-  while (cont) {
-    size_t len = 0;
-    getline(&code, &len, stdin); assert(code != NULL);
-    if (strcmp(",q\n", code)) {
-      Struct* e = chars_to_struct(code);
-      code = NULL;
-      if (e == NULL) {
-        fprintf(stderr, "Parse error\n");
-      } else {
-        Map* envOut = NULL;
-        e = evalTopLevel(env, envOut, e);
-        if (e != NULL) { printValue(stdout, e); printf("\n"); }
-        env = envOut;
-      }
+  if (argc > 1) {
+    code = argv[1];
+    Struct* e = chars_to_struct(code);
+    if (e == NULL) {
+      fprintf(stderr, "Parse error\n");
     } else {
-      cont = false;
+      Map* envOut = NULL;
+      e = evalTopLevel(env, envOut, e);
+      if (e != NULL) { printValue(stdout, e); printf("\n"); }
+    }
+  } else {
+    while (cont) {
+      size_t len = 0;
+      getline(&code, &len, stdin); assert(code != NULL);
+      if (strcmp(",q\n", code)) {
+        Struct* e = chars_to_struct(code);
+        code = NULL;
+        if (e == NULL) {
+          fprintf(stderr, "Parse error\n");
+        } else {
+          Map* envOut = NULL;
+          e = evalTopLevel(env, envOut, e);
+          if (e != NULL) { printValue(stdout, e); printf("\n"); }
+          env = envOut;
+        }
+      } else {
+        cont = false;
+      }
     }
   }
   return 0;
